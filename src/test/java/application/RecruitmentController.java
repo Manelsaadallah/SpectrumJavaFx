f2576cc016a1cb5b9bbdb7ece2f8263c35ed7cda
Rick Lionel <45133928+ricko97@users.noreply.github.com>
2019-04-03 04:08:08 +0100

    work updated

--- src/test/java/application/RecruitmentController.java
+++ src/test/java/application/RecruitmentController.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.awt.Desktop.Action;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -15,7 +16,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import com.gluonhq.charm.glisten.control.ToggleButtonGroup;
-import com.jfoenix.controls.JFXAlert;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
@@ -29,6 +29,7 @@ import com.jfoenix.controls.JFXTextField;
 import entities.Candidacy;
 import entities.CandidacyStatus;
 import entities.Candidate;
+import entities.Interview;
 import entities.JobOffer;
 import entities.Question;
 import entities.Test;
@@ -66,21 +67,37 @@ public class RecruitmentController implements Initializable {
 	@FXML
 	private JFXButton btn_newQuestion,btn_editQuestion,brn_deleteQuestion,btn_addTest,btn_deleteTest;
 	@FXML
-	private Pane pane_offers,pane_candidacies,pane_interviews,pane_tests,pane_addOffer,pane_viewCand;
+	private JFXButton btn_testAddQuestion,btn_testRemoveQuestion,btn_testSave,btn_testCancel;
 	@FXML
-	private JFXListView<Label>lv_offers,lv_questions;
+	private Pane pane_offers,pane_candidacies,pane_interviews,pane_tests,pane_addOffer,pane_viewCand,pane_addTest;
+	@FXML
+	private JFXListView<Label>lv_offers,lv_questions,lv_testQuestions;
 	@FXML
 	private JFXDatePicker dp_startDate,dp_endDate,dp_nOffStart,dp_nOffEnd;
 	@FXML
 	private JFXTextArea ta_description,ta_nOffDescription,ta_question;
 	@FXML
-	private JFXTextField tf_nOffTitle,tf_choice1,tf_choice2,tf_choice3;
+	private JFXTextField tf_nOffTitle,tf_choice1,tf_choice2,tf_choice3,tf_answer;
+	@FXML
+	private JFXTextField tf_testDuration,tf_testQuestion,tf_testAnswer,tf_testChoice1,tf_testChoice2,tf_testChoice3;
 	@FXML
 	private Label lbl_offer;
 	@FXML
 	private JFXRadioButton rb_allOffers,rb_currentOffers,rb_allCand,rb_acceptedCand,rb_rejectedCand,rb_pendingCand;
 	@FXML
+	private JFXRadioButton rb_allInterview,rb_comingInterview;
+	@FXML
 	private TableView<Candidacy>tv_viewCand,tv_cand;
+	@FXML
+	private TableView<Interview>tv_interviews,tv_testResults;
+	@FXML
+	private TableColumn<Interview,String>tc_resultCandidate,tc_resultTest,tc_resultPassed,tc_candidateInterview,tc_testInterview;
+	@FXML
+	private TableColumn<Interview,Date>tc_dateInterview;
+	@FXML
+	private TableColumn<Interview,Integer>tc_idInterview;
+	@FXML
+	private TableColumn<Interview,Float>tc_resultScore;
 	@FXML
 	private TableColumn<Candidacy,String>tc_name,tc_birth,tc_offerTitle,tc_nameCand,tc_emailCand,tc_birthCand;
 	@FXML
@@ -90,11 +107,11 @@ public class RecruitmentController implements Initializable {
 	@FXML
 	private JFXComboBox<String> cbx_cand;
 	@FXML
-	private JFXComboBox<Test_t>cbx_tests;
+	private JFXComboBox<Test_t>cbx_tests,cbx_testType;
 	@FXML
 	private ToggleButtonGroup tbg_cand;
 	
-	
+	public static List<Question>tempQuestions = new ArrayList<Question>();
 	private String jndiname = "spectrum-ear/spectrum-ejb/JobOfferService!services_cand_interv."
 			+ "JobOfferServiceRemote";
 	private String jndiname2 ="spectrum-ear/spectrum-ejb/CandidacyService!services_cand_interv."
@@ -144,7 +161,13 @@ public class RecruitmentController implements Initializable {
 			
 		}
 		else if (btn_editOffer.getText().equals("Save")) {
-			simpleDialog(rootPane, "You are in edit mode, finish the changes first", "Ok");
+			simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
+		}
+		else if (btn_editQuestion.getText().equals("Save")) {
+			simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
+		}
+		else if (btn_newQuestion.getText().equals("Save")) {
+			simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
 		}
 		else if(e.getSource()==btn_showOffers) {
 			pane_offers.toFront();
@@ -162,11 +185,18 @@ public class RecruitmentController implements Initializable {
 			pane_candidacies.toFront();
 		}
 		else if(e.getSource()==btn_showInterviews) {
+			loadInterviews(getInterviews());
+			loadResults(getInterviews());
 			currentPane = pane_interviews;
 			pane_interviews.toFront();
 		}
 		else if(e.getSource()==btn_showTests) {
-			//cbx_test.getItems().clear();
+			ta_question.clear();
+			tf_answer.clear();
+			tf_choice1.clear();
+			tf_choice2.clear();
+			tf_choice3.clear();
+			cbx_tests.getItems().clear();
 			for (Test test : loadTests()) {
 				cbx_tests.getItems().add(test.getType());
 			}
@@ -364,7 +394,7 @@ public class RecruitmentController implements Initializable {
 		ObservableList<Candidacy>candidacies = FXCollections.observableArrayList();
 		for (Candidacy candidacy : candidacy_s.getCandidaciesByOffer(currentOffer.getId())) {
 			Candidate candidate = candidacy.getCandidate();
-			candidacy.setName(candidate.getUser().getName());
+			candidacy.setName(candidate.getUser().getUsername());
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			candidacy.setBirth(dateFormat.format(candidate.getUser().getBirth()));
 			candidacy.setEmail(candidate.getUser().getEmail());
@@ -500,10 +530,11 @@ public class RecruitmentController implements Initializable {
 	}
 	public void printQuestions(Test test) throws NamingException{
 		lv_questions.getItems().clear();
-		for (Question question : test.getQuestions()) {
-			Label lbl = new Label(question.getContent());
-			lv_questions.getItems().add(lbl);
-		}
+			for (Question question : test.getQuestions()) {
+				Label lbl = new Label(question.getContent());
+				lv_questions.getItems().add(lbl);
+			}
+		
 	}
 	
 	@FXML
@@ -512,5 +543,320 @@ public class RecruitmentController implements Initializable {
 		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
 		printQuestions(interview_s.searchTest(EntId, cbx_tests.getValue()));
 	}
+	@FXML
+	public void onSelectQuestion(MouseEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		Question q = interview_s.searchQuestion(interview_s.searchTest(EntId, cbx_tests.getValue()).getId(),
+				lv_questions.getSelectionModel().getSelectedItem().getText());
+		ta_question.setText(q.getContent());
+		tf_answer.setText(q.getAnswer());
+		tf_choice1.setText(q.getChoice1());
+		tf_choice2.setText(q.getChoice2());
+		tf_choice3.setText(q.getChoice3());
+	}
+	@FXML
+	public void editQuestion(ActionEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		if (btn_editQuestion.getText().equals("Edit")) {
+			if (lv_questions.getSelectionModel().isEmpty())
+				simpleDialog(rootPane, "Select the question you want to edit", "Ok");
+			else if (btn_newQuestion.getText().equals("Save"))
+				simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
+			else {
+				btn_editQuestion.setText("Save");
+				ta_question.setEditable(true);
+				tf_answer.setEditable(true);
+				tf_choice1.setEditable(true);
+				tf_choice2.setEditable(true);
+				tf_choice3.setEditable(true);
+			}
+		}else {
+			Question currentQuestion =interview_s.searchQuestion(interview_s.searchTest(EntId, cbx_tests.getValue()).getId(),
+					lv_questions.getSelectionModel().getSelectedItem().getText());
+			currentQuestion.setContent(ta_question.getText());
+			currentQuestion.setAnswer(tf_answer.getText());
+			currentQuestion.setChoice1(tf_choice1.getText());
+			currentQuestion.setChoice2(tf_choice2.getText());
+			currentQuestion.setChoice3(tf_choice3.getText());
+			interview_s.modifyQuestion(currentQuestion);
+			btn_editQuestion.setText("Edit");
+			ta_question.setEditable(false);
+			tf_answer.setEditable(false);
+			tf_choice1.setEditable(false);
+			tf_choice2.setEditable(false);
+			tf_choice3.setEditable(false);
+			printQuestions(interview_s.searchTest(EntId, cbx_tests.getValue()));
+		}
+	}
+	@FXML
+	public void removeQuestion(ActionEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		if (lv_questions.getSelectionModel().isEmpty()) {
+			simpleDialog(rootPane, "Select the question you want to delete", "Ok");
+		}else if (btn_editQuestion.getText().equals("Save")) {
+			simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
+		}else if (btn_newQuestion.getText().equals("Save")) {
+			simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
+		}else {
+			Test test = interview_s.searchTest(EntId, cbx_tests.getValue());
+			Question currentQuestion = interview_s.searchQuestion(test.getId(),lv_questions.getSelectionModel().getSelectedItem().getText());
+			JFXDialogLayout dialogLayout = new JFXDialogLayout();
+			JFXButton no = new JFXButton("No");
+			JFXButton delete = new JFXButton("Delete");
+			JFXDialog dialog = new JFXDialog(rootPane,dialogLayout,JFXDialog.DialogTransition.TOP);
+			delete.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent)-> {
+				interview_s.removeQuestion(currentQuestion);
+				dialog.close();
+				try {
+					printQuestions(interview_s.searchTest(EntId, cbx_tests.getValue()));
+				} catch (NamingException e1) {
+					e1.printStackTrace();
+				}
+			});
+			no.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent)-> {
+				dialog.close();
+			});
+			dialogLayout.setHeading(new Label("Do you really want to delete this question ?"));
+			dialogLayout.setActions(no,delete);
+			dialog.show();
+		}
+	}
+	@FXML
+	public void addQuestion(ActionEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		if (btn_newQuestion.getText().equals("New")) {
+			if (btn_editQuestion.getText().equals("Save"))
+				simpleDialog(rootPane, "You are in edit mode, save the changes first", "Ok");
+			else if(cbx_tests.getValue()==null)
+			simpleDialog(rootPane, "Please choice a test", "Ok");
+			else {
+				ta_question.clear();
+				tf_answer.clear();
+				tf_choice1.clear();
+				tf_choice2.clear();
+				tf_choice3.clear();
+				btn_newQuestion.setText("Save");
+				ta_question.setEditable(true);
+				tf_answer.setEditable(true);
+				tf_choice1.setEditable(true);
+				tf_choice2.setEditable(true);
+				tf_choice3.setEditable(true);
+			}	
+		}else {
+			if(ta_question.getText().equals("")||tf_answer.getText().equals("")||tf_choice1.getText().equals("")||
+					tf_choice2.getText().equals("")||tf_choice3.getText().equals("")) {
+				simpleDialog(rootPane, "Required fields not filled", "Ok");
+			}
+			else{
+				Question question = new Question();
+				question.setContent(ta_question.getText());
+				question.setAnswer(tf_answer.getText());
+				question.setChoice1(tf_choice1.getText());
+				question.setChoice2(tf_choice2.getText());
+				question.setChoice3(tf_choice3.getText());
+				interview_s.addQuestionToTest(interview_s.searchTest(EntId, cbx_tests.getValue()).getId(), question);
+				btn_newQuestion.setText("New");
+				ta_question.setEditable(false);
+				tf_answer.setEditable(false);
+				tf_choice1.setEditable(false);
+				tf_choice2.setEditable(false);
+				tf_choice3.setEditable(false);
+				printQuestions(interview_s.searchTest(EntId, cbx_tests.getValue()));
+			}
+		}
+	}
+	@FXML
+	public void newTest(ActionEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		if(interview_s.getAllTest(EntId).size()>=3)
+			simpleDialog(rootPane, "You got all test added", "Ok");
+		else {
+			pane_addTest.toFront();
+			cbx_testType.getItems().clear();
+			lv_testQuestions.getItems().clear();
+			cbx_testType.getItems().add(Test_t.Technical);
+			cbx_testType.getItems().add(Test_t.Linguistic);
+			cbx_testType.getItems().add(Test_t.Psychotechnical);
+			for (Test test : interview_s.getAllTest(EntId)) {
+				if (test.getType()==Test_t.Technical||test.getType()==Test_t.Linguistic
+						||test.getType()==Test_t.Psychotechnical)
+					cbx_testType.getItems().remove(test.getType());
+				else
+					cbx_testType.setValue(test.getType());
+			}
+			currentPane = pane_addTest;
+		}
 		
+	}
+	@FXML
+	public void testAddQuestion(ActionEvent e) {
+		Question question = new Question ();
+		if(tf_testQuestion.getText().equals("")||tf_testAnswer.getText().equals("")||tf_testChoice1.getText().equals("")||
+				tf_testChoice2.getText().equals("")||tf_testChoice3.getText().equals("")) {
+			simpleDialog(rootPane, "Required fields not filled", "Ok");
+		}else {
+			question.setContent(tf_testQuestion.getText());
+			question.setAnswer(tf_testAnswer.getText());
+			question.setChoice1(tf_testChoice1.getText());
+			question.setChoice2(tf_testChoice2.getText());
+			question.setChoice3(tf_testChoice3.getText());
+			tempQuestions.add(question);
+			lv_testQuestions.getItems().add(new Label(question.getContent()));
+			tf_testQuestion.clear();
+			tf_testAnswer.clear();
+			tf_testChoice1.clear();
+			tf_testChoice2.clear();
+			tf_testChoice3.clear();
+		}
+		
+	}
+	public Question getQuestionByContent(String content) {
+		for (Question question : tempQuestions) {
+			if (question.getContent().equals(content))
+				return question;
+		}
+		return null;
+	}
+	@FXML
+	public void testRemoveQuestion(ActionEvent e) {
+		if (lv_testQuestions.getSelectionModel().isEmpty()) {
+			simpleDialog(rootPane, "Select the question you want to remove", "Ok");
+		}else {
+			Question question = getQuestionByContent(lv_testQuestions.getSelectionModel().getSelectedItem().getText());
+			lv_testQuestions.getItems().remove(lv_testQuestions.getSelectionModel().getSelectedItem());
+			tempQuestions.remove(question);
+		}
+	}
+	@FXML
+	public void saveTest(ActionEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		if(tf_testDuration.getText().isEmpty())
+			simpleDialog(rootPane, "Required fields not filled", "Ok");
+		else if(tempQuestions.isEmpty()||lv_testQuestions.getItems().isEmpty())
+			simpleDialog(rootPane, "You need to add at least one question", "Ok");
+		else if (!isInteger(tf_testDuration.getText()))
+			simpleDialog(rootPane, "Please enter a valid duration", "Ok");
+		else if (cbx_testType.getValue()==null)
+			simpleDialog(rootPane, "Please select the test type", "Ok");
+		else {
+			Test test = new Test();
+			test.setType(cbx_testType.getValue());
+			test.setDuration(Integer.parseInt(tf_testDuration.getText()));
+			test=interview_s.addTest(test, EntId);
+			for (Question question : tempQuestions) {
+				interview_s.addQuestionToTest(test.getId(), question);
+			}
+			for (Test tst : loadTests()) {
+				cbx_tests.getItems().add(tst.getType());
+			}
+			pane_tests.toFront();
+			currentPane = pane_tests;
+		}
+			
+	}
+	@FXML
+	public void deleteTest(ActionEvent e) throws NamingException {
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		if (cbx_tests.getValue()==null)
+			simpleDialog(rootPane, "Please select a test to delete", "Ok");
+		else {
+			Test test = interview_s.searchTest(EntId, cbx_tests.getValue());
+			interview_s.deleteTest(test);
+			cbx_tests.getItems().clear();
+			for (Test tst : loadTests()) {
+				cbx_tests.getItems().add(tst.getType());
+			}
+		}
+	}
+		
+	@FXML
+	public void cancelAddTest(ActionEvent e) throws NamingException {
+		JFXDialogLayout dialogLayout = new JFXDialogLayout();
+		JFXButton no = new JFXButton("No");
+		JFXButton yes = new JFXButton("Yes");
+		JFXDialog dialog = new JFXDialog(rootPane,dialogLayout,JFXDialog.DialogTransition.TOP);
+		yes.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent)-> {
+			dialog.close();
+			tf_testDuration.clear();
+			lv_testQuestions.getItems().clear();
+			pane_tests.toFront();
+			currentPane = pane_tests;
+			
+		});
+		no.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent)-> {
+			dialog.close();
+		});
+		dialogLayout.setHeading(new Label("Modifications not saved, continue ?"));
+		dialogLayout.setActions(no,yes);
+		dialog.show();
+	}
+	public static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    } catch(NullPointerException e) {
+	        return false;
+	    }
+	    return true;
+	}
+	//------------------------------------------TESTS END------------------------------------------------------
+	
+		//------------------------------------------------INTERVIEW PART----------------------------------------------------
+		
+	
+	public void loadInterviews(ObservableList<Interview>list) throws NamingException {
+		tv_interviews.getItems().clear();
+		tc_candidateInterview.setCellValueFactory(new PropertyValueFactory<>("candidate"));
+		tc_testInterview.setCellValueFactory(new PropertyValueFactory<>("test"));
+		tc_idInterview.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tc_dateInterview.setCellValueFactory(new PropertyValueFactory<>("date"));
+		tv_interviews.setItems(list);
+	}
+	public void loadResults(ObservableList<Interview>list) throws NamingException {
+		tv_testResults.getItems().clear();
+		tc_resultCandidate.setCellValueFactory(new PropertyValueFactory<>("candidate"));
+		tc_resultTest.setCellValueFactory(new PropertyValueFactory<>("test"));
+		tc_resultScore.setCellValueFactory(new PropertyValueFactory<>("score"));
+		tc_resultPassed.setCellValueFactory(new PropertyValueFactory<>("passed"));
+		tv_testResults.setItems(list);
+	}
+	public ObservableList<Interview>getInterviews() throws NamingException{
+		Context context = new InitialContext();
+		InterviewServiceRemote interview_s = (InterviewServiceRemote ) context.lookup(jndiname3);
+		ObservableList<Interview>interviews = FXCollections.observableArrayList();
+		for (Interview interview : interview_s.getAllInterviews()) {
+			Interview interv = new Interview();
+			interv.setId(interview.getId());
+			interv.setDate(interview.getDate());
+			interv.setCandidate(interview.getTestResult().getCandidate().getUser().getUsername());
+			interv.setTest(interview.getTestResult().getTest().getType());
+			interv.setScore(interview.getTestResult().getScore());
+			interv.setPassed(interview.getTestResult().getPassed() ? "Yes": "No");
+			interviews.add(interv);
+		}
+		return interviews;
+	}
+	@FXML
+	public void sortInterview(ActionEvent e) throws NamingException {
+		ObservableList<Interview>interviews = FXCollections.observableArrayList();
+		if (rb_allInterview.isSelected()) {
+			interviews = getInterviews();
+			loadInterviews(interviews);
+		}else {
+			for (Interview interview : getInterviews()) {
+				if(interview.getDate().compareTo(new Date())>0)
+					interviews.add(interview);
+			}
+			loadInterviews(interviews);
+		}
+	}
+	
 }
